daa274dc0e0f81c1296181625cd2a804f8fc27e5
Sunny <loganayers2013@yahoo.com>
2019-10-17 21:06:40 -0500

    Updates

---
+++ precache-manifest.f824c0ecb67214e7d91edd0010644201.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3e1c9fda9fec051f8236568d6f3f25",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "c89c53a8d1c20f2efaaa",
+    "url": "/react-memory-game/static/css/main.5065ab2c.chunk.css"
+  },
+  {
+    "revision": "3b0b06228a17c96a2c93",
+    "url": "/react-memory-game/static/js/2.587f8899.chunk.js"
+  },
+  {
+    "revision": "c89c53a8d1c20f2efaaa",
+    "url": "/react-memory-game/static/js/main.68fffe9f.chunk.js"
+  },
+  {
+    "revision": "a0908d8d29639206d617",
+    "url": "/react-memory-game/static/js/runtime-main.5c648d34.js"
+  }
+]);
